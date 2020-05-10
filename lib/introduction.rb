741c2e243ec0ee1054945d39df390ac0f3cc3d38
Luis Leandro <luchrist777@gmail.com>
2020-05-10 23:14:48 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,6 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language (arg_one,age_two)
+  puts "Hi, my name is #{arg_one} and I am learning to program in #{age_two}."
+end
