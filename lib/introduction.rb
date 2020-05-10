8910e892c8aab8703867ee87344379bc02fbfd72
Luis Leandro <luchrist777@gmail.com>
2020-05-10 22:55:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-def introduction_with_language (name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language (arg_one,age_two)
+  puts "Hi, my name is #{arg_one} and I am learning to program in #{age_two}."
 end
